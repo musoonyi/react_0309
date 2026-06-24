88b58d4503f6897ea58a183158f33c684e8f4eb0
musoonyi <musoonyi@gmail.com>
2026-06-24 10:40:19 +0900

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,68 @@
 import React, { useState } from 'react'
 
+const headerNav=[
+  {
+    title: "intro",
+    url: "/",
+  },
+  {
+    title: "skill",
+    url: "/",
+  },
+  {
+    title: "site",
+    url: "/",
+  },
+  {
+    title: "port",
+    url: "/",
+  },
+  {
+    title: "contact",
+    url: "/",
+  },
+]
+
 
 const Header = () => {
 
-  const [show,setShow] = useState(false);
-  // useState(): component 의 상태를 관리해주는 훅
-  // false : 초기값
-  
-  //들어온 값의 반대값으로 바꿔주는 함수
-  const toggleMenu = ()=>{
-    setShow((pShow) => !pShow);
-    //! : 반대값 참->거짓 거짓->참
-  }
+    const [show,setShow] = useState(false);
+    // show : false 
+
+    const toggleMenu=()=>{
+      setShow((pShow)=>!pShow);
+      //들어온 입력값을 0 -> 1, 1 -> 0 으로 변경해주는 함수
+    }
 
 
-  return (
-    <header id='header' role='banner'>
-      <section className='header_inner'>
-        <h1 className='header_logo'>
-          <a href="/">Portfolio <em>react.js</em></a>
-        </h1>
-        <nav className={`header_nav ${show? "show":""}`} role='navigation' aria-label='800이상 메뉴'>
-          <ul>
-            <li><a href="/">intro</a></li>
-            <li><a href="/">skill</a></li>
-            <li><a href="/">site</a></li>
-            <li><a href="/">port</a></li>
-            <li><a href="/">contact</a></li>
-          </ul>
-        </nav>
-        <nav className='header_nav_m' id='headerToggle' role='button' tabIndex={0} onClick={toggleMenu}>
-          <span></span>
-        </nav>
-      </section>
-    </header>
-  )
+
+    return (
+        <header id='header' role='banner'>
+            <section className='header_inner'>
+                <h1 className="header_logo">
+                    <a href="/">portfolio <em>react.js</em></a>
+                </h1>
+                <nav className={`header_nav ${show? "show":""}`} role='navigation' aria-label='800이상 메뉴'>
+                    <ul>
+                      {headerNav.map((nav,key)=>(
+                        <li key={key}><a href={nav.url}>{nav.title}</a></li>
+                      ))}                      
+                    </ul>
+                </nav>
+                <nav 
+                    className="header_nav_m" 
+                    id='headerToggle'
+                    role='button'
+                    tabIndex={0}
+                    onClick={toggleMenu}
+                    //클릭했을 때  toggleMenu 함수 실행해라.
+                    aria-expanded={show? "true":"false"}
+                >
+                    <span></span>
+                </nav>
+            </section>
+        </header>
+    )
 }
 
 export default Header
