9838ecbfd196446a4ea601e2dfed6e99a943e337
musoonyi <musoonyi@gmail.com>
2026-06-24 11:43:49 +0900

    Add files via upload

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import intro_img from "../assets/img/intro.png"
+//이미지를 불러오는 형식
 
 const Intro = () => {
   return (
-    <div>
-      Intro
-    </div>
+    <section id='intro'>
+      <div className="intro_inner">
+        <h2 className='intro_title'>port developer</h2>
+        <article className='intro_lines' aria-hidden="true"> 
+  {/* aria-hidden"true": 리더기에서 읽어주지 않게 막아주는 명령어 */}
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+        </article>
+
+        <article className='intro_text'>
+          <div className="text">
+            <div>talent is</div>
+            <div>found at the end of the</div>
+            <div>effort</div>
+          </div>
+          <div className="img">
+            <img src={intro_img} alt="인트로이미지" />
+          </div>
+        </article>
+
+        <article className='intro_lines bottom' aria-hidden="true"> 
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+        </article>
+      </div>
+    </section>
   )
 }
 
